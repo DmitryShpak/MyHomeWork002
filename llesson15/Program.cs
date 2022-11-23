f2e32b47a1807479c6ecbb23396d4a139559e834
Karaton <karaton@inbox.ru>
2022-11-23 15:29:48 +0300

    change 14,15

--- llesson15/Program.cs
+++ llesson15/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 15 Hello, World!");
+﻿int[] CreatArray()
+{
+    int[] ints = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < ints.Length; i++) { ints[i] = random.Next(); }
+return ints;
+}
+
+string GetArrayAsString(int[] array) 
+{
+    string resalt = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        resalt += $"{array[i].ToString()}, ";
+    }
+
+    return resalt;  
+}
+
+Console.WriteLine(GetArrayAsString(CreatArray()));
